f69cf3ca5341bab08fb1ecd9799d80def0041053
jmcwilliams403 <jmcwilliams403@gmail.com>
2022-07-01 15:57:10 -0400

    Use 2D array for map

--- core/src/com/rombus/evilbones/raycaster/Map.java
+++ core/src/com/rombus/evilbones/raycaster/Map.java
@@ -2,17 +2,18 @@ package com.rombus.evilbones.raycaster;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Map {
     protected int size;
-    protected int[] wallGrid;
+    protected int[][] wallGrid;
     protected double light;
     protected Texture skybox;
     protected Texture wallTexture;
 
     public Map(int size) {
         this.size = size;
-        this.wallGrid = new int[this.size * this.size];
+        this.wallGrid = new int[this.size][this.size];
         this.light = 0.5;
         this.skybox = new Texture(Gdx.files.internal("deathvalley_panorama.jpg"));
         this.wallTexture = new Texture(Gdx.files.internal("wall_texture.jpg"));
@@ -22,12 +23,14 @@ public class Map {
         x = Math.floor(x);
         y = Math.floor(y);
         if (x < 0 || x > this.size - 1 || y < 0 || y > this.size - 1) return -1;
-        return this.wallGrid[(int) (y * this.size + x)];
+        return this.wallGrid[(int)x][(int)y];
     }
 
     public void randomize() {
-        for (int i = 0; i < this.size * this.size; i++) {
-            this.wallGrid[i] = Math.random() < 0.3 ? 1 : 0;
+        for (int x = 0; x < this.size; x++) {
+        	for (int y = 0; y < this.size; y++) {
+        		this.wallGrid[x][y] = MathUtils.randomBoolean(0.3f)? 1 : 0;
+        	}
         }
     }
 
