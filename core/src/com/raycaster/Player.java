531bead2788b6c9f2cf8887f7c889748b98bfd11
jmcwilliams403 <jmcwilliams403@gmail.com>
2022-07-14 18:04:35 -0400

    slow down paces if real distance traveled is less

--- core/src/com/raycaster/Player.java
+++ core/src/com/raycaster/Player.java
@@ -29,10 +29,16 @@ public class Player {
         this.direction = (this.direction + angle + Math.PI*2) % (Math.PI*2);
     }
 
-    public void walk(double distance, Map map, double direction) {
-        final double dx = Math.cos(direction) * distance, dy = Math.sin(direction) * distance;
+    public double walk(double distance, Map map, double direction) {
+        double dx = Math.cos(direction) * distance, dy = Math.sin(direction) * distance;
+        
         if (map.get(this.x + dx, this.y) <= 0) this.x += dx;
+        else dx = 0;
+        
         if (map.get(this.x, this.y + dy) <= 0) this.y += dy;
+        else dy = 0;
+        
+        return Math.hypot(dx, dy);
     }
 
     public void update(Controls controls, Map map, double seconds) {
@@ -40,11 +46,12 @@ public class Player {
 
         if (controls.move) {
 	        final double distance = this.speed * seconds;
-	        if (controls.left) this.walk(distance, map, this.direction - Math.PI/2);
-	        if (controls.right) this.walk(distance, map, this.direction + Math.PI/2);
-	        if (controls.forward) this.walk(distance, map, this.direction);
-	        if (controls.backward) this.walk(distance, map, this.direction + Math.PI);
-	        this.paces = (this.paces + distance) % (Math.PI*2);
+	        double delta = 0;
+	        if (controls.left) delta = Math.max(delta, this.walk(distance, map, this.direction - Math.PI/2));
+	        if (controls.right) delta = Math.max(delta, this.walk(distance, map, this.direction + Math.PI/2));
+	        if (controls.forward) delta = Math.max(delta, this.walk(distance, map, this.direction));
+	        if (controls.backward) delta = Math.max(delta, this.walk(distance, map, this.direction + Math.PI));
+	        this.paces = (this.paces + delta) % (Math.PI*2);
         }
         else {
         	this.paces = 0;
