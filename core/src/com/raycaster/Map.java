e620173e5f1bcc892c6f60fc50c6359e71fb450e
jmcwilliams403 <jmcwilliams403@gmail.com>
2022-07-15 13:36:32 -0400

    use default vWrap for skybox

--- core/src/com/raycaster/Map.java
+++ core/src/com/raycaster/Map.java
@@ -17,7 +17,7 @@ public class Map {
         this.wallGrid = new int[this.size][this.size];
         this.light = 32;
         this.skybox = new Texture(Gdx.files.internal("panorama.png"));
-        this.skybox.setWrap(TextureWrap.Repeat, TextureWrap.MirroredRepeat);
+        this.skybox.setWrap(TextureWrap.Repeat, TextureWrap.ClampToEdge);
         this.wallTexture = new Texture(Gdx.files.internal("wall.png"));
         this.floorTexture = new Texture(Gdx.files.internal("floor.png"));
     }
