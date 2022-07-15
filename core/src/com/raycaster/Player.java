f57f7220fef2e82e2e3136c67f3f8f88a3b218eb
jmcwilliams403 <jmcwilliams403@gmail.com>
2022-07-15 14:48:46 -0400

    Simplify Player.paces reset math

--- core/src/com/raycaster/Player.java
+++ core/src/com/raycaster/Player.java
@@ -57,9 +57,9 @@ public class Player {
         }
         else if (this.paces > 0) {
             double closer = (Math.abs(this.paces - Math.PI) > Math.PI/2)? 0 : Math.PI;
-            double delta = ((closer - this.paces + Math.PI*3) % (Math.PI*2)) - Math.PI;
-            this.paces = (this.paces + delta * distance + Math.PI*2) % (Math.PI*2);
-            if(Math.abs(this.paces - closer) <= 0x1p-8) this.paces = 0;
+            double delta = (((Math.PI*2 - this.paces) % (Math.PI*2)) - closer) * distance;
+            this.paces = (this.paces + delta + Math.PI*2) % (Math.PI*2);
+            if(Math.abs(this.paces - closer) < 0x1p-8) this.paces = 0;
         }
     }
 }
