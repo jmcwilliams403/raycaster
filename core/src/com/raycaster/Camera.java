8cc66065261e2fb1a286fbc651f3451e77717a4d
jmcwilliams403 <jmcwilliams403@gmail.com>
2022-07-17 16:55:07 -0400

    rename Camera.drawColumns.x to delta

--- core/src/com/raycaster/Camera.java
+++ core/src/com/raycaster/Camera.java
@@ -128,8 +128,8 @@ public class Camera implements Disposable {
 
 	private void drawColumns(Player player, Map map, float ambient) {
 		for (int column = 0; column < this.resolution; column++) {
-			double x = (1 + column) / this.resolution;
-			double angle = this.fov * (0.5 - (Math.atan2(1 - x, x) / η));
+			double delta = (1 + column) / this.resolution;
+			double angle = this.fov * (0.5 - (Math.atan2(1 - delta, delta) / η));
 			Ray ray = new Ray(map, player.x, player.y, player.direction + angle, this.range);
 			Texture texture = map.wallTexture;
 			int left = (int) Math.floor(column * this.spacing);
